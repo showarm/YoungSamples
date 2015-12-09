a850d72936764d2c8551708a34d86035fb5a526c
chenshaosina <chenshaosina@chenshaosinadeMacBook-Pro.local>
2015-12-09 16:13:00 +0800

    弹幕RollingTextView

--- app/src/main/java/info/ipeanut/youngsamples/third/view/ThirdViewActivity.java
+++ app/src/main/java/info/ipeanut/youngsamples/third/view/ThirdViewActivity.java
@@ -13,6 +13,7 @@ import info.ipeanut.youngsamples.SampleAdapter;
 import info.ipeanut.youngsamples.SampleBean;
 import info.ipeanut.youngsamples.third.view.mtextview.MTextViewActivity;
 import info.ipeanut.youngsamples.third.view.percentProgressView.PercentProgressActivity;
+import info.ipeanut.youngsamples.third.view.rollingTextView.RollingActivity;
 
 /**
  * Created by chenshaosina on 15/12/8.
@@ -21,12 +22,14 @@ public class ThirdViewActivity extends ListActivity {
 
     private static String[] names = {
             "解决TextView乱换行并支持图文混排",
-            "百分比环形进度条"
+            "百分比环形进度条",
+            "弹幕"
 
     };
     private static Class[] cls = {
             MTextViewActivity.class,
-            PercentProgressActivity.class
+            PercentProgressActivity.class,
+            RollingActivity.class
 
     };
     private List<SampleBean> samples = null;
